4d5d7ab53bdb3e54b663d148caaf1dd3bbd36960
Alisher Ortiqov <mcpeblocker@gmail.com>
2022-04-20 20:31:01 +0500

    initial class

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-console.log("Salom");
-//# sourceMappingURL=index.js.map
---
+++ src/Fint.ts
@@ -0,0 +1,14 @@
+class Fint {
+    private token;
+
+    constructor(
+        token: String,
+        options?: {}
+    ) {
+        this.token = token;
+    }
+
+    private call(method: String, payload: {}) {
+        // TODO: Fetch Telegram Bot API
+    }
+}
